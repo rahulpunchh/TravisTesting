f925fdb384c90f38288c44c8f67614df83cffd58
rahulpunchh <rahul@punchh.com>
2016-01-13 18:32:00 +0530

    Hello Again Try-7->remove Term Line

--- app/src/main/java/com/example/punchh/hellotravistesting/MainActivity.java
+++ app/src/main/java/com/example/punchh/hellotravistesting/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.punchh.hellotravistesting;
 
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
     }
 
     @Override
