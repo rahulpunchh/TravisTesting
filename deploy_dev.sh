3b7543ba713fd9b97d29f32ba05762bb654ac45d
rahulpunchh <rahul@punchh.com>
2016-01-18 13:03:34 +0530

    Hockey App integration-> deployment changes deploty.sh added-try another version

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -3,5 +3,5 @@ curl \
 -F "status=2" \
 -F "notify=0" \
 -F "ipa=@TravisTesting/build/outputs/apk/dev-debug.apk" \
--H "X-HockeyAppToken: 2ef71f14ee5c41febf9f40e7a8f829a6 " \
-https://rink.hockeyapp.net/api/2/apps/2ef71f14ee5c41febf9f40e7a8f829a6/app_versions/upload
+-H "X-HockeyAppToken: d3a1d30e769b4c45a12e09a979778f1a" \
+https://rink.hockeyapp.net/api/2/apps/2ef71f14ee5c41febf9f40e7a8f829a6/1/upload
