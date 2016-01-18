579e116704823491229fc95264a96bc394e6dee4
rahulpunchh <rahul@punchh.com>
2016-01-18 13:21:49 +0530

    dev to app debug

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -2,6 +2,6 @@
 curl \
 -F "status=2" \
 -F "notify=0" \
--F "ipa=@0/home/ubuntu/TravisTesting/app/build/outputs/apk/dev-debug.apk" \
+-F "ipa=@TravisTesting/app/build/outputs/apk/app-debug.apk" \
 -H "X-HockeyAppToken: d3a1d30e769b4c45a12e09a979778f1a" \
 https://rink.hockeyapp.net/api/2/apps/2ef71f14ee5c41febf9f40e7a8f829a6/1/upload
